34216816c780875ea061338f51865b1fc444f94b
leetaejun <leetaejun147@gmail.com>
2017-01-17 10:07:45 +0900

    modify e5

--- src/com/taejun/e5/Main.java
+++ src/com/taejun/e5/Main.java
@@ -10,6 +10,24 @@ import java.util.*;
  * https://www.youtube.com/watch?v=ipT2XG1SHtQ&t=67s
  */
 public class Main {
+    // [[[들어가기]]]
+    // # Generics 가 뭔가요?
+    // 컴파일러를 잘 쓰기 위한 도구?
+    // type parameter based polymorphism?
+    // Generics 을 안쓰면 에러가 나더라? 경고가 나더라?
+
+    // # p149. Generics 덕에 프로그램이 더 안전해 지는 이유는 무엇인가?
+    // 그 덕에 프로그램은 더 안전하고 명료해지지만, 대가가 있다. 프로그램이 복잡해지는 것이다.
+
+    // Generics 나오기 전과 후는 어떻게 달라졌는지?
+
+    // Generics 없이는 자바는 형안정성이 없는 언어일까?
+    // static type 언어 => 컴파일 타임에 타입이 틀리면 예외를 던져준다.
+    // strong type 언어 => 런타임 시점에도 타입이 틀리면 예외를 던져준다.
+    // 근데 Generics 전에는 지원하지 않고 Object를 넣고 Casting을 했다.
+    // (자바 초기에 구현 당시 어려움이 있고 사용상 힘들다고 판단하여 뺐음)
+
+    /*
     public static void main(String[] args) {
         List<int[]> a = new ArrayList<>();
         int[] aa = {1,2,3,4,5};
@@ -28,6 +46,7 @@ public class Main {
         s2.add("ccc"); s2.add("ddd");
         Set<String> s3 = union(s1, s2);
     }
+    */
 
     private static void unsafeAdd(List list, Object o) {
         list.add(o);
@@ -49,17 +68,12 @@ public class Main {
         */
     }
 
-    // [[[들어가기]]]
-    // # Generics 가 뭔가요?
-    // 컴파일러를 잘 쓰기 위한 도구?
-    // type parameter based polymorphism?
-    // Generics 을 안쓰면 에러가 나더라? 경고가 나더라?
 
-    // # p149. Generics 덕에 프로그램이 더 안전해 지는 이유는 무엇인가?
-    // 그 덕에 프로그램은 더 안전하고 명료해지지만, 대가가 있다. 프로그램이 복잡해지는 것이다.
-
-    // Generics 나오기 전과 후는 어떻게 달라졌는지?
-        /*
+    // 규칙23
+    // Don`t use raw types in new code
+    // 새 코드에는 무인자 제네릭 자료형을 사용하지 말라
+    /*
+    public static void main(String[] args) {
         List aList = new ArrayList();
         List<String> bList = new ArrayList<>();
         List<Object> cList = new ArrayList<>();
@@ -70,21 +84,10 @@ public class Main {
         cList.add("a");
         cList.add(1);
 
-         unsafeAdd(bList, new Integer(1));
-         unsafeAdd2(bList, new Integer(2));
-         */
-
-    // Generics 없이는 자바는 형안정성이 없는 언어일까?
-    // static type 언어 => 컴파일 타임에 타입이 틀리면 예외를 던져준다.
-    // strong type 언어 => 런타임 시점에도 타입이 틀리면 예외를 던져준다.
-    // 근데 Generics 전에는 지원하지 않고 Object를 넣고 Casting을 했다.
-    // (자바 초기에 구현 당시 어려움이 있고 사용상 힘들다고 판단하여 뺐음)
-
-
-    // 규칙23
-    // Don`t use raw types in new code
-    // 새 코드에는 무인자 제네릭 자료형을 사용하지 말라
-
+        unsafeAdd(bList, new Integer(1));
+        unsafeAdd2(bList, new Integer(2));
+    }
+    */
 
     // 규칙24
     // (Eliminate unchecked warning)
@@ -106,16 +109,51 @@ public class Main {
     // List<Object> objectList = new ArrayList<String>();
     // String의 List는 Object의 List와 상속관계가 아니기 때문에 안된다.
 
-    //
 
     // 규칙26
     // Favor generic types
     // 가능하면 제네릭 자료형으로 만들 것
+    /** @see Stack */
 
     // 규칙27
     // Favor generic methods
     // 가능하면 제네릭 메소드로 만들 것
     // Class generic vs Method generic
+    static class Generics<T> {
+        T print(T t) {
+            System.out.println(t);
+            return t;
+        }
+
+        // static 메소드에서도 가능할까?
+        /*
+        static T print(T t) {
+            return t;
+        }
+        */
+
+        // 사용법
+        static <S, T extends S> boolean copyList(List<S> targetList, List<T> addList) {
+            return targetList.addAll(addList);
+        }
+    }
+    /* 위의 사용 예
+    public static void main(String[] args) {
+        List<A> aList = new ArrayList<>();
+        List<B> bList = new ArrayList<>();
+        aList.add(new A());
+        bList.add(new B());
+        Generics.copyList(aList, bList);
+        System.out.println(aList);
+    }
+    static class A {
+
+    }
+    static class B extends A {
+
+    }
+    */
+    // 이어서
     // 타입 추론 가능
     // 형 안정성? => 컴파일이 되면 믿을 수 있게 돌아간다
     public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
@@ -131,27 +169,29 @@ public class Main {
     // 불공변 자료형이 문제가 되는 경우(위의 union처럼)가 있다-> 좀 더 유연성있게 만들기 위한게 한정적 와일드카드
     // wildcards라고 해서 다 넣는게 아니라 null 빼곤 다 안됨(p.155)
     // PECS stands for producer-extends, consumer-super.
+    public static void main(String[] args) {
+        List<Number> aList = new ArrayList<>();
+        aList.add(1);
+        //List<Integer> bList = aList; // ?
+        List<Integer> bList = new ArrayList<>();
+        List<String> cList = new ArrayList<>();
+        /*
+        printMe(bList);
+        printMe(cList);
+        printMe2(bList);
+        printMe2(cList);
+        */
+    }
+    private static void printMe (List<?> numbers) {
+        //numbers.forEach(s -> System.out.println(s));
+        //numbers.add(new Object());
+    }
+    private static void printMe2 (List<? extends Number> numbers) {
+        //numbers.forEach(s -> System.out.println(s));
+    }
 
     // 규칙29
     // Consider typesafe heterogeneous containers
     // 형 안전 다형성 컨테이너를 쓰면 어떨지 따져보라
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
